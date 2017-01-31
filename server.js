e9498d7568241c1853ec0e8c551de60e348b74eb
Chris Stapler <cstapler@iastate.edu>
2017-01-30 20:40:35 -0600

    Fix broken comment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-/ set the port of our application
+// set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
 
