d1d9a44eec025f24f14b0fa1e722bc0cae5ebfcc
Chris Stapler <cstapler@iastate.edu>
2017-01-30 20:22:55 -0600

    Change the way port is set

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express')
 var app = express()
 
+/ set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 
-app.use(express.static('public'));
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function(){
+app.listen(port, function(){
   console.log("CrossTheRoad is running on port 3000!")
 });
